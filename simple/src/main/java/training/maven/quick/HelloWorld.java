a0f0e479954d1aad097650db3dee3cc162ab2969
Jason Taylor <jason@screencasts.pro>
2015-02-15 20:57:47 -0500

    Renaming project folders

--- simple/src/main/java/training/maven/quick/HelloWorld.java
+++ simple/src/main/java/training/maven/quick/HelloWorld.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
+package training.maven.quick;
 
-    // method main(): ALWAYS the APPLICATION entry point
+public class HelloWorld {
+	
     public static void main (String[] args) {
 		System.out.println ("Hello World!");
     }
